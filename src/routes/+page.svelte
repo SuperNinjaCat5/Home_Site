cd5b6720eecefd68c18b97e21680d583c7c4046a
SuperNinjaCat5 <ben.elliott.2021@gmail.com>
2025-07-24 13:22:21 -0400

    chore: initialize SvelteKit project with TypeScript and Tailwind CSS
    - Added svelte.config.js for SvelteKit configuration with vitePreprocess and adapter-auto.
    - Created tsconfig.json extending SvelteKit's default configuration with strict type checking and module resolution settings.
    - Set up vite.config.ts to include Tailwind CSS and SvelteKit plugins for Vite.

---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<head>
+  <title>SuperNinjaCat5.us</title>
+</head>
+
+<style>
+  .centeredX {
+    display: flex;
+    justify-content: center;
+  }
+  h1 {
+    font-size: 3rem;
+    margin: 2rem 0;
+  }
+</style>
+
+<img src="/images/1.png" alt="Loading Error" />
+<img src="/images/2.png" alt="Loading Error" />
---
+++ src/routes/noway/+page.svelte
@@ -0,0 +1,3 @@
+<embed 
+src="https://hc-cdn.hel1.your-objectstorage.com/s/v3/0191b6f35e0e452ef4b1ff02375099e903f7b3b9_shockedguyguytwitterreactionmeme_greensc.mp4" 
+type="video/mp4" width="1920" height="1080" style="w-full h-full object-cover" />
