c97c80770ea56d77bd72d7c60edbc867b09b60b4
SuperNinjaCat5 <ben.elliott.2021@gmail.com>
2025-07-24 19:47:11 -0400

    Add Lora font-face definition to fonts.css

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,49 @@
-<head>
+
+
+<svelte:head>
   <title>SuperNinjaCat5.us</title>
-</head>
+  <link rel="stylesheet" href="/fonts.css" />
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@400..900&display=swap" rel="stylesheet">
+</svelte:head>
 
 <style>
-  .centeredX {
-    display: flex;
-    justify-content: center;
-  }
-  h1 {
-    font-size: 3rem;
-    margin: 2rem 0;
+    .glitch {
+      animation: glitch 2s infinite;
+    }
+
+     @keyframes glitch {  /*Credit to https://github.com/hackclub/hackducky/blob/main/site/Current/src/index.css (hackducky ysws) cuz it inspired the site and I lifted this anim cuz i suck at anims :sob:*/
+      0%, 100% { transform: translate(0); }
+      20% { transform: translate(-2px, 2px); }
+      40% { transform: translate(-2px, -2px); }
+      60% { transform: translate(2px, 2px); }
+      80% { transform: translate(2px, -2px); }
+    }
+
+    .background {
+    background: #030303;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
   }
 </style>
 
-<img src="/images/1.png" alt="Loading Error" />
-<img src="/images/2.png" alt="Loading Error" />
+<div class="background">
+  <div style="display: flex; justify-content: center; margin-top: 2rem;">
+    <svg viewBox="0 0 500 80" width="100%" preserveAspectRatio="xMidYMid meet" xmlns="http://www.w3.org/2000/svg" class="glitch">
+        <defs>
+          <filter id="neonGlow" x="-50%" y="-50%" width="200%" height="200%">
+            <feDropShadow dx="0" dy="0" stdDeviation="3" flood-color="#00ff22" flood-opacity="0.8"/>
+            <feDropShadow dx="0" dy="0" stdDeviation="5" flood-color="#00ff22" flood-opacity="0.6"/>
+          </filter>
+        </defs>
+      <text x="250" y="50" font-family="Orbitron" fill="#00ff22" stroke="black" stroke-width="0" text-anchor="middle" dominant-baseline="middle">
+        <tspan font-size="50" filter="url(#neonGlow)">SuperNinjaCat5</tspan>
+        <tspan font-size="20" dy="-15" dx="5" stroke-width="0" filter="url(#neonGlow)">.us</tspan>
+      </text>
+    </svg>
+  </div>
+</div>
