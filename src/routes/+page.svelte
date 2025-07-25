24efb55123d0db6c0924cf816603dcfdab175d86
SuperNinjaCat5 <ben.elliott.2021@gmail.com>
2025-07-24 20:12:04 -0400

    Update content and structure in +page.svelte for enhanced user introduction and engagement

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -28,11 +28,14 @@
     left: 0;
     width: 100vw;
     height: 100vh;
+    z-index: -1;
   }
 </style>
 
-<div class="background">
-  <div style="display: flex; justify-content: center; margin-top: 2rem;">  <!-- SuperNinjaCat5.us title -->
+<div class="background"></div>
+
+<div>
+  <div style="display: flex; justify-content: center; margin-top: 2rem; overflow: hidden;">  <!-- SuperNinjaCat5.us title -->
     <svg viewBox="0 0 500 80" width="100%" preserveAspectRatio="xMidYMid meet" xmlns="http://www.w3.org/2000/svg" class="glitch">
         <defs>
           <filter id="neonGlow" x="-50%" y="-50%" width="200%" height="200%">
@@ -47,11 +50,35 @@
     </svg>
   </div>
     <div style="display: flex; justify-content: center; margin-top: 2rem;">
-      <p style="text-align: center; max-width: 80%; color: #00ff22; background-color: #001a00; padding: 1.5rem 2rem; border-radius: 12px; box-shadow: 0 0 15px #00ff22aa; font-family: 'Orbitron', sans-serif; font-size: 1.1rem; line-height: 1.6; border: 1.5px solid #00ff22;">
-        Big paragraphBig paragraphBig paragraphBig paragraphBig paragraphBig paragraphBig paragraphBig paragraph
-        Big paragraphBig paragraphBig paragraphBig paragraphBig paragraphBig paragraphBig paragraphBig paragraph
-        Big paragraphBig paragraphBig paragraphBig paragraphBig paragraphBig paragraphBig paragraphBig paragraph
-        Big paragraphBig paragraphBig paragraphBig paragraphBig paragraphBig paragraphBig paragraphBig paragraph
+      <p style="max-width: 80%; color: #00ff22; background-color: #001a00; padding: 1.5rem 2rem; border-radius: 12px; box-shadow: 0 0 15px #00ff22aa; font-family: 'Orbitron', sans-serif; font-size: 1.1rem; line-height: 1.6; border: 1.5px solid #00ff22;">
+        Hi, I'm @SuperNinjaCat5<br>
+        <br>
+        - 🐍 Am speak Python and English  <br>
+        - 🔥 Proud vibe coder  <br>
+        - 🎮 Big fan of video games <br> 
+        - 🐱 Unbiasedly believes cats are the best animals <br>  
+        - Plays 🎺 but big and long  <br>
+        <br>
+        ---<br>
+        <br>
+        About Me <br>
+        <br>
+        I'm passionate about coding and gaming, I enjoy learning useless things (3.141592653589793238462643383279502). Currently focused on Python projects and getting free stuff from hcb lol.
+        <br>
+        ---
+        <br>
+        What I'm Working On
+        <br>
+        - Learning more advanced Python concepts<br>  
+        - Building small games and tools  <br>
+        <br>
+        ---<br>
+        <br>
+        Get in Touch<br>
+        <br>
+        📫 How to reach me: contact@superninjacat5.us
+        <br>
+        ---
       </p>
     </div>
 </div>
